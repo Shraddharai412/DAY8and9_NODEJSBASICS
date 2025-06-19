957941daeb34e1da865909206207c2b634ac34fc
Shraddharai412 <shraddharai412@gmail.com>
2025-06-19 14:53:56 +0530

    file.js Updated

--- node/file.js
+++ node/file.js
@@ -2,41 +2,41 @@ const fs=require('fs');
 
 
 // Task 1: Read to a file
-// fs.readFile('filemodules.txt','utf8',(err,data)=>{
-//     if(err){
-//         console.error('Error reading file:', err);
-//         return;
-//     }
-//     console.log('File content:', data);
-// });
+fs.readFile('filemodules.txt','utf8',(err,data)=>{
+    if(err){
+        console.error('Error reading file:', err);
+        return;
+    }
+    console.log('File content:', data);
+});
 
-//task2 : Write to a file
-// fs.writeFile('filemodules.txt','This is a new content for the file.',(err)=>{
-//     if(err){
-//         console.error('Error writing to file:', err);
-//         return;
-//     }
-//     console.log('File written successfully.');
-// });
+// task2 : Write to a file
+fs.writeFile('filemodules.txt','This is a new content for the file.',(err)=>{
+    if(err){
+        console.error('Error writing to file:', err);
+        return;
+    }
+    console.log('File written successfully.');
+});
 
-//task 3 delete a file
-// fs.unlink('filemodules.txt',(err)=>{
-//     if(err){
-//         console.error('Error deleting file:', err);
-//         return;
-//     }
-//     console.log('File deleted successfully.');
-// });
+// task 3 delete a file
+fs.unlink('filemodules.txt',(err)=>{
+    if(err){
+        console.error('Error deleting file:', err);
+        return;
+    }
+    console.log('File deleted successfully.');
+});
 
 
 // task 4 creating same file
-// fs.writeFile('filemodules.txt','This is a new content for the file.',(err)=>{
-//     if(err){
-//         console.error('Error writing to file:', err);
-//         return;
-//     }
-//     console.log('File written successfully.');
-// });
+fs.writeFile('filemodules.txt','This is a new content for the file.',(err)=>{
+    if(err){
+        console.error('Error writing to file:', err);
+        return;
+    }
+    console.log('File written successfully.');
+});
 
 //task 5 : deleting a file after 5 seconds
 fs.writeFile('log.txt','This is a log file.',(err)=>{
