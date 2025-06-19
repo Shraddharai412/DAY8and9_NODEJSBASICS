9ec737ef091b8439f2f9dae2c59266ccab5c5d6e
Shraddharai412 <shraddharai412@gmail.com>
2025-06-19 14:54:30 +0530

    sample.js Updated

--- node/sample.js
+++ node/sample.js
@@ -1,68 +1,68 @@
 //task 1
 
-// function mul(a,b)
-// {
-//     return a * b;
-// }
-// console.log(mul(2,3)); 
+function mul(a,b)
+{
+    return a * b;
+}
+console.log(mul(2,3)); 
 
 ///task 2
 
-// var x=10;
-// console.log(typeof x);
+var x=10;
+console.log(typeof x);
 
 //task 3 fs
 
-// var fs=require("fs");
+var fs=require("fs");
 
-// fs.writeFileSync("sample.txt","welcome to node.js");
-// fs.appendFileSync("sample.txt","\nThis is a sample file.");
+fs.writeFileSync("sample.txt","welcome to node.js");
+fs.appendFileSync("sample.txt","\nThis is a sample file.");
 
-// const data=fs.readFileSync("sample.txt","utf-8");
-// console.log(data);
+const data=fs.readFileSync("sample.txt","utf-8");
+console.log(data);
 
-// fs.unlinkSync("sample.txt")
+fs.unlinkSync("sample.txt")
 
 //task 4 fs
 
-// var js=require("fs");
-// js.writeFileSync("sample1.js","console.log('Hello, World!');\n");
-// js.appendFileSync("sample1.js","console.log('This is a sample file.');\n");
+var js=require("fs");
+js.writeFileSync("sample1.js","console.log('Hello, World!');\n");
+js.appendFileSync("sample1.js","console.log('This is a sample file.');\n");
 
 
 // const data=js.readFileSync("sample1.js","utf-8");
-// console.log(data);
+console.log(data);
 
-// js.unlinkSync("sample1.js");
+js.unlinkSync("sample1.js");
 
 //task 5 path
 
-// var path = require("path");
-// console.log(path.basename(__filename));
-// console.log(path.dirname(__filename));
-// console.log(path.extname(__filename));
+var path = require("path");
+console.log(path.basename(__filename));
+console.log(path.dirname(__filename));
+console.log(path.extname(__filename));
 
-// console.log(path.join(__dirname, "folder","sample.txt"));
+console.log(path.join(__dirname, "folder","sample.txt"));
 
 //task6
 
-// var os = require("os");
-// console.log("OS Type: " + os.type());
-// console.log("OS Platform: " + os.platform());
-// console.log("OS Architecture: " + os.arch());
-// console.log("Total Memory: " + os.totalmem() + " bytes");
-// console.log("Free Memory: " + os.freemem() + " bytes");
-// console.log("Home Directory: " + os.homedir());
-// console.log("Temporary Directory: " + os.tmpdir());
-// console.log("Hostname: " + os.hostname());
-// console.log("Network Interfaces: ", os.networkInterfaces());
-// console.log("Uptime: " + os.uptime() + " seconds");
-// console.log("Load Average: ", os.loadavg());
-// console.log("CPU Information: ", os.cpus());
-// console.log("Release: " + os.release());
+var os = require("os");
+console.log("OS Type: " + os.type());
+console.log("OS Platform: " + os.platform());
+console.log("OS Architecture: " + os.arch());
+console.log("Total Memory: " + os.totalmem() + " bytes");
+console.log("Free Memory: " + os.freemem() + " bytes");
+console.log("Home Directory: " + os.homedir());
+console.log("Temporary Directory: " + os.tmpdir());
+console.log("Hostname: " + os.hostname());
+console.log("Network Interfaces: ", os.networkInterfaces());
+console.log("Uptime: " + os.uptime() + " seconds");
+console.log("Load Average: ", os.loadavg());
+console.log("CPU Information: ", os.cpus());
+console.log("Release: " + os.release());
 
 
 //core modules fs, path, os
-//task 7
+
 
 
